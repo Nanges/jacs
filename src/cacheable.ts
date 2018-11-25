62929c0dd452341b1ca4a43fdd7d925ae6b87426
unknown <3200283+Nanges@users.noreply.github.com>
2018-11-25 22:41:25 +0100

    Refactor cacheable

--- src/cacheable.ts
+++ src/cacheable.ts
@@ -7,12 +7,13 @@ export function cacheable<T>(config?:T){
         /**
          * One cacheable manager per target instance
          */
-        const cacheableSymbol = Symbol(`cacheable_${propertyKey}`);
+        const cacheMngrSymbol = Symbol(`cacheMngr_${propertyKey}`);
         const original = descriptor.value as (...args:any[]) =>Observable<any>;
+        const factory = CacheManagerResolver.factory;
 
         descriptor.value = function(...args:any[]){
             /** Setup cachemanager once */
-            const cacheManager = this[cacheableSymbol] || (this[cacheableSymbol] = CacheManagerResolver.factory(config));
+            const cacheManager = this[cacheMngrSymbol] || (this[cacheMngrSymbol] = factory(config));
             const executable = original.bind(this, ...args) as Executable<any>;
             return cacheManager.execute(executable, args);
         }
