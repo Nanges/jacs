1bcf47ec8cc46cddbd21559bec9f6ed0f1c26bd4
LAHAYE Olivier <olahaye@irisad.brussels>
2018-12-03 18:03:44 +0100

    Set cacheable more generic

--- src/cacheable.ts
+++ src/cacheable.ts
@@ -2,20 +2,21 @@ import { Observable } from "rxjs";
 import { Executable } from "./executable";
 import { CacheManagerResolver } from "./cache-manager-resolver";
 
-export function cacheable<T>(config?:T){
-    return (target, propertyKey: string, descriptor: PropertyDescriptor) => {
-        /**
-         * One cacheable manager per target instance
-         */
-        const cacheMngrSymbol = Symbol(`cacheMngr_${propertyKey}`);
-        const original = descriptor.value as (...args:any[]) =>Observable<any>;
-        const factory = CacheManagerResolver.factory;
+export function cacheable(config?: any) {
+  return (target, propertyKey: string, descriptor: PropertyDescriptor) => {
+    /**
+     * One cacheable manager per target instance
+     */
+    const cacheMngrSymbol = Symbol(`cacheMngr_${propertyKey}`);
+    const original = descriptor.value as (...args: any[]) => Observable<any>;
+    const factory = CacheManagerResolver.factory;
 
-        descriptor.value = function(...args:any[]){
-            /** Setup cachemanager once */
-            const cacheManager = this[cacheMngrSymbol] || (this[cacheMngrSymbol] = factory(config));
-            const executable = original.bind(this, ...args) as Executable<any>;
-            return cacheManager.execute(executable, args);
-        }
-    }
+    descriptor.value = function(...args: any[]) {
+      /** Setup cachemanager once */
+      const cacheManager =
+        this[cacheMngrSymbol] || (this[cacheMngrSymbol] = factory(config));
+      const executable = original.bind(this, ...args) as Executable<any>;
+      return cacheManager.execute(executable, args);
+    };
+  };
 }
