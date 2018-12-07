b8f5bd644ee31d7f0092d51e9ece3eacb084c8c3
LAHAYE Olivier <olahaye@irisad.brussels>
2018-12-07 19:55:46 +0100

    Fix with inflight observable and switchMap

--- src/cache-content.ts
+++ src/cache-content.ts
@@ -1,88 +1,145 @@
-import { Observable, of, Subject, Subscription } from "rxjs";
-import { tap, switchMap, finalize } from "rxjs/operators";
-import { Executable } from "./executable";
-import { cloneDeep } from "lodash";
+import { Observable, of, Subject, Subscription, Observer, race } from 'rxjs';
+import { tap, switchMap, finalize, takeUntil } from 'rxjs/operators';
+import { Executable } from './executable';
+import { cloneDeep } from 'lodash';
 
 export class CacheContent<T> {
-  private valid: boolean;
-  private subject: Subject<any>;
-  private subscription: Subscription;
+    private _valid: boolean;
+    private _pending: boolean;
+    private subject: Subject<any>;
+    private onCancel: Subject<void>;
+    private src$: Observable<T>;
 
-  constructor(private _value: T = null) {
-    this.valid = this._value != null;
-  }
-
-  /**
-   * Prevent value mutation by cloning it
-   */
-  get value(): T {
-    return cloneDeep(this._value) as T;
-  }
-
-  /**
-   * Return an observable of the cached value or the given fallback if cache is invalid
-   * @param fallback : The fallback used to refresh the value when the cache is not valid anymore
-   */
-  public get(fallback: Executable<T>): Observable<T> {
-    // return of({}).pipe(
-    //   switchMap(() => {
-    //     if (this.valid) {
-    //       return of(this._value);
-    //     }
-
-    //     if (this.subject == null) {
-    //       this.subject = new Subject<T>();
-    //       return fallback().pipe(
-    //         tap(c => this.updateCache(c)),
-    //         tap(c => this.notifyInflightObservers(c), e => this.handleError(e))
-    //       );
-    //     }
-
-    //     return this.subject.asObservable();
-    //   })
-    // );
-
-    if (this.valid) {
-      return of(this._value);
+    constructor(private _value: T = null) {
+        this._valid = this._value != null;
     }
 
-    if (this.subject == null) {
-      this.subject = new Subject<T>();
-      
-      return fallback().pipe(
-        tap(c => this.updateCache(c)),
-        tap(c => this.notifyInflightObservers(c), e => this.handleError(e)),
-        finalize(() => console.log('done'))
-      );
+    /**
+     * Prevent value mutation by cloning it
+     */
+    get value(): T {
+        return cloneDeep(this._value) as T;
     }
 
-    return this.subject.asObservable();
-  }
-
-  /**
-   * Invalidate the cache.
-   */
-  public invalidate(): void {
-    this.valid = false;
-  }
-
-  private updateCache(content: T) {
-    this._value = content;
-    this.valid = true;
-  }
-
-  private notifyInflightObservers(content: T) {
-    if (this.subject.observers.length > 0) {
-      this.subject.next(content);
+    /**
+     *
+     */
+    get valid(): boolean {
+        return this._valid;
     }
-    // dispose the subject as soon as possible
-    this.subject.complete();
-    this.subject = null;
-  }
 
-  private handleError(e: any) {
-    this.subject.error(e);
-    this.subject.complete();
-    this.subject = null;
-  }
+    /**
+     *
+     */
+    get pending(): boolean {
+        return this._pending;
+    }
+
+    /**
+     * Return an observable of the cached value or the given fallback if cache is invalid
+     * @param fallback : The fallback used to refresh the value when the cache is not valid anymore
+     */
+    public get(fallback: Executable<T>): Observable<T> {
+        if (this.valid) {
+            return of(this._value);
+        }
+
+        //
+        this.src$ = fallback().pipe(
+            tap(c => this.updateCache(c)),
+            tap(c => this.notifyInflightObservers(c))
+        );
+
+        // first subscription will set the pending state to true
+        if (!this.pending) {
+            this._pending = true;
+            return Observable.create((o: Observer<T>) => {
+                this.src$.subscribe(o);
+                return () => this.handleUnSubscription();
+            });
+        }
+
+        console.log('inflight mode');
+        // inflight observable
+        if (!this.subject) {
+            this.subject = new Subject<T>();
+        }
+
+        if (!this.onCancel) {
+            this.onCancel = new Subject<void>();
+        }
+
+        return Observable.create((o: Observer<T>) => {
+            console.log('inflight subscription');
+            const cancelSrc$ = this.onCancel.pipe(
+                tap(() => console.log('cancelled')),
+                switchMap(() => this.src$)
+            );
+            race(cancelSrc$, this.subject).subscribe(o);
+        });
+
+        // return Observable.create((o: Observer<T>) => {
+        //     if (!this.onCancel) {
+        //         this.onCancel = new Subject<void>();
+        //     }
+
+        //     const transit$ = this.onCancel.pipe(
+        //         tap(() => console.log('cancel')),
+        //         switchMap(() => this.src$)
+        //     );
+        //     const t$ = race(transit$, this.subject);
+        //     t$.subscribe(o);
+        // });
+    }
+
+    /**
+     * Invalidate the cache.
+     */
+    public invalidate(): void {
+        this._valid = false;
+    }
+
+    private updateCache(content: T) {
+        this._value = content;
+        this._valid = true;
+        this._pending = false;
+    }
+
+    private notifyInflightObservers(content: T) {
+        if (this.subject && this.subject.observers.length > 0) {
+            this.subject.next(content);
+        }
+
+        // dispose the subject as soon as possible
+        this.disposeSubject();
+    }
+
+    // private handleError(e: any) {
+    //     this.subject.error(e);
+    //     this.disposeSubject();
+    // }
+
+    private handleUnSubscription() {
+        console.log('handle cancellation');
+        if (!this.valid && this.subject) {
+            console.log(this.onCancel.observers.length);
+            this.onCancel.next(void 0);
+        } else {
+            this._pending = false;
+        }
+
+        /*if (this.onCancel) {
+            console.log(this.subject.observers.length);
+            this.onCancel.next(void 0);
+        } else {
+            this.disposeSubject();
+        }*/
+    }
+
+    private disposeSubject() {
+        if (this.subject) {
+            this.subject.complete();
+            this.subject = null;
+        }
+    }
 }
--- test/cache-content.spec.ts
+++ test/cache-content.spec.ts
@@ -1,67 +1,63 @@
-import { assert } from "chai";
-import { CacheContent } from "../src/cache-content";
-import { delay, mergeMap, tap, switchMap, finalize, concatMap, takeUntil } from "rxjs/operators";
-import { MockService, Operation } from "./mock-service";
-import { Executable } from "src/executable";
-import { interval, timer, of, Subject, BehaviorSubject } from "rxjs";
+import { assert } from 'chai';
+import { CacheContent } from '../src/cache-content';
+import { delay, switchMap, concatMap } from 'rxjs/operators';
+import { MockService, Operation } from './mock-service';
+import { Executable } from 'src/executable';
+import { Subject } from 'rxjs';
 
 describe('Cache content', () => {
+    let cacheContent: CacheContent<Operation>;
+    let service: MockService;
 
-    let cacheContent:CacheContent<Operation>;
-    let service:MockService;
-    
     beforeEach(() => {
         cacheContent = new CacheContent<Operation>();
         service = new MockService();
     });
 
-    xdescribe('get() method', () => {
+    describe('get() method', () => {
+        it('should return expected data', done => {
+            const call1 = service.getValue.bind(service, 0) as Executable<Operation>;
 
-        it('should return expected data', (done) => {
+            cacheContent.get(call1).subscribe(v => {
+                assert.equal(v.data.name, 'John');
+                done();
+            });
+        });
 
-            const call1 = service.getValue.bind(service,0) as Executable<Operation>;
+        it('should cache the value of the first observable parameter', done => {
+            const call1 = service.getValue.bind(service, 0) as Executable<Operation>;
+            const call2 = service.getValue.bind(service, 1) as Executable<Operation>;
 
-            cacheContent.get(call1)
-                .subscribe(v => {
-                    assert.equal(v.data.name, 'John');
-                    done();
-                });
-        })
-
-        it('should cache the value of the first observable parameter', (done) => {
-
-            const call1 = service.getValue.bind(service,0) as Executable<Operation>;
-            const call2 = service.getValue.bind(service,1) as Executable<Operation>;
-
-            cacheContent.get(call1)
-                .pipe(
-                    mergeMap(() => cacheContent.get(call2))
-                )
+            cacheContent
+                .get(call1)
+                .pipe(switchMap(() => cacheContent.get(call2)))
                 .subscribe(v => {
                     assert.notEqual(v.data.name, 'Jack');
                     assert.equal(v.data.name, 'John');
                     done();
                 });
         });
+    });
 
-        xit('should return observable using source once', (done) => {
-            const source = service.getValue.bind(service, 0) as Executable<Operation>;
-            const cC = cacheContent.get(source);
+    describe('call id and subscription features', () => {
+        it('call, cancel, call', done => {
+            const call = service.getValue.bind(service, 1, 10) as Executable<Operation>;
+            const sub = cacheContent.get(call).subscribe();
+            sub.unsubscribe();
+            cacheContent.get(call).subscribe(v => done());
+        });
 
-            cC.subscribe(v => {
-                assert.equal(v.id, 1);
-            });
+        it('call, call, cancel', done => {
+            const call = service.getValue.bind(service, 1, 10) as Executable<Operation>;
 
-            cC.pipe(delay(20))
-            .subscribe(v => {
-                assert.equal(v.id, 1);
-                done();
-            })
-        })
+            const sub = cacheContent.get(call).subscribe();
+            const sub2 = cacheContent.get(call).subscribe(() => done());
+            sub.unsubscribe();
+        });
     });
 
     describe('inflight observable', () => {
-        it('should use inflight observable feature', (done) => {
+        it('should use inflight observable feature', done => {
             const source = service.getValue.bind(service, 1, 10) as Executable<Operation>;
 
             cacheContent.get(source).subscribe(v => {
@@ -70,129 +66,75 @@ describe('Cache content', () => {
 
             cacheContent.get(source).subscribe(v => {
                 assert.equal(v.id, 1);
-                setTimeout(() => done(), 10);
+                setTimeout(() => done(), 5);
             });
         });
 
-        it('should work with unsubscription', (done) => {
+        it('should work with switchMap', done => {
             // source with a delay of 10ms
             const source = service.getValue.bind(service, 1, 10) as Executable<Operation>;
 
-            const sub = cacheContent.get(source).subscribe();
-            sub.unsubscribe();
-            cacheContent.get(source).subscribe(v => {
-                assert.equal(v.id, 1);
+            const emitter = new Subject();
+
+            emitter.pipe(switchMap(() => cacheContent.get(source))).subscribe(v => {
+                assert.equal(v.id, 2);
                 done();
             });
-        });
 
-        it('test', (done) => {
-            // source with a delay of 10ms
-            const src$ = of('5');
-            const emitter = new BehaviorSubject(null);
-
-            src$.subscribe(emitter);
-            emitter.subscribe(v => console.log(v));
-            
-
-            done();
-        });
-
-        xit('should work with switchMap', (done) => {
-            // source with a delay of 10ms
-            const source = service.getValue.bind(service, 1, 10) as Executable<Operation>;
-
-            // emit every 5ms until 11ms
-            const src$ = interval(5)
-            .pipe(
-                switchMap(() => cacheContent.get(source)),
-                takeUntil(timer(15))
-            ).subscribe(v =>{
-                assert.equal(v.id, 1); 
-                done();
-            });
-        });
-
-        xit('should use inflight observable feature when multiple subscriptions on returned source', (done) => {
-            
-            const source = service.getValue.bind(service, 2, 10)  as Executable<Operation>;
-            const cC = cacheContent.get(source);
-
-            cC.subscribe(v => {
-                assert.equal(v.id, 1);
-            });
-
-            cC.subscribe(v => {
-                assert.equal(v.id, 1);
-                setTimeout(() => done(), 10);
-            });
+            emitter.next(0);
+            setTimeout(() => {
+                emitter.next(0);
+            }, 5);
         });
     });
 
-    xdescribe('cache invalidation', () => {
-        it('should invalidate the cache', (done) => {
-
+    describe('cache invalidation', () => {
+        it('should invalidate the cache', done => {
             const call1 = service.getValue.bind(service, 0) as Executable<Operation>;
             const call2 = service.getValue.bind(service, 1) as Executable<Operation>;
 
-            cacheContent.get(call1).pipe(
-                concatMap(() => {
-                    cacheContent.invalidate();
-                    return cacheContent.get(call2);
-                })
-            ).subscribe(v => {
-                assert.notEqual(v.data.name, 'John');
-                assert.equal(v.data.name, 'Jack');
-                done();
-            });
-        });
-
-        xit('should use source twice when invalidation between', (done) => {
-            const source = service.getValue.bind(service, 0) as Executable<Operation>;
-            const cC = cacheContent.get(source);
-
-            cC.subscribe(v => {
-                assert.equal(v.id, 1);
-                cacheContent.invalidate();
-            });
-
-            cC.pipe(delay(20))
-            .subscribe(v => {
-                assert.equal(v.id, 2);
-                done();
-            })
-        });
-    });
-
-    xdescribe('default cache', () => {
-        it('use default value', (done) => {
-            let cacheContent = new CacheContent<string|Operation>('foo');
-            cacheContent.get(service.getValue.bind(service, 0) as Executable<string|Operation>)
+            cacheContent
+                .get(call1)
+                .pipe(
+                    concatMap(() => {
+                        cacheContent.invalidate();
+                        return cacheContent.get(call2);
+                    })
+                )
                 .subscribe(v => {
-                    assert.notEqual(v, 'bar');
-                    assert.equal(v, 'foo');
+                    assert.notEqual(v.data.name, 'John');
+                    assert.equal(v.data.name, 'Jack');
                     done();
                 });
         });
     });
 
-    xdescribe('value accessor', () => {
-        it('use value accessor', (done) => {
+    describe('default cache', () => {
+        it('use default value', done => {
+            let cacheContent = new CacheContent<string | Operation>('foo');
+            cacheContent.get(service.getValue.bind(service, 0) as Executable<string | Operation>).subscribe(v => {
+                assert.notEqual(v, 'bar');
+                assert.equal(v, 'foo');
+                done();
+            });
+        });
+    });
+
+    describe('value accessor', () => {
+        it('use value accessor', done => {
             let cacheContent = new CacheContent<Operation>();
-            cacheContent.get(service.getValue.bind(service, 0) as Executable<Operation>)
-                .subscribe(v => {
-                    assert.deepEqual(v , cacheContent.value);
-                    done();
-                });
+            cacheContent.get(service.getValue.bind(service, 0) as Executable<Operation>).subscribe(v => {
+                assert.deepEqual(v, cacheContent.value);
+                done();
+            });
         });
 
-        it('should value accessor be a clone of original cached value', (done) => {
+        it('should value accessor be a clone of original cached value', done => {
             let cacheContent = new CacheContent<Operation>();
-            cacheContent.get(service.getValue.bind(service, 0) as Executable<Operation>)
-                .subscribe(v => {
-                    assert.notEqual(v , cacheContent.value);
-                    done();
-                });
+            cacheContent.get(service.getValue.bind(service, 0) as Executable<Operation>).subscribe(v => {
+                assert.notEqual(v, cacheContent.value);
+                done();
+            });
         });
-    })
+    });
 });
